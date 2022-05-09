219991f16830000cc5a035d9cb1ea73fdf6f443f
DimitriGLLN <d.guillon@online-platforms>
2022-05-09 12:07:35 +0200

    feature(project): Initialization of the project

---
+++ src/main/java/com/example/quiode/QuiodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiodeApplication.class, args);
+	}
+
+}
